965003cc176cd3aa11a0d0bce40206659b1499be
CodingRock-Star <dhirajdafouti2020@gmail.com>
2024-10-25 23:45:23 +0530

    added

--- app/src/main/java/com/android/datastructure/SlidingWindow.kt
+++ app/src/main/java/com/android/datastructure/SlidingWindow.kt
@@ -1,19 +1,35 @@
 package com.android.datastructure
 
 fun main() {
-    val nums = intArrayOf(1, 2, 4, 5, 6, 7, 12)
-    val range = 6
-    slidingWindowSumWithRange(nums, range)
+    val nums = mutableListOf(1, 2, 4, 5, 6, 7, 12)
+    val range = 4
+    println("Maximum Array ${slidingWindowSumWithRange(nums, range)}")
 }
 
-fun slidingWindowSumWithRange(nums: IntArray, range: Int): Int {
+fun slidingWindowSumWithRange(nums: MutableList<Int>, range: Int): Int {
     if (nums.isEmpty()) return 0
-    if(nums.size==1) return nums[0]
+    if (nums.size == 1) return nums[0]
 
     var start = 0
     var end = nums.size-1
-    while (start < end){
+    var increment=0
+    var rangeTo=range
+    var sumArray = mutableListOf(end)
+    while (start < end) {
 
+        var sum = 0
+        for (i in start..rangeTo) {
+            sum += nums[i]
+        }
+
+        if (rangeTo == end) {
+            return sumArray.max()
+        } else {
+            sumArray.add(start,sum)
+            start=++increment
+            rangeTo += 1
+
+        }
     }
     return 0
 }
